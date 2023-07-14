4d16a184e6857e8507fbf87302a227f39cb0aaea
davittsiskarashvili <datotsiskarashvili@gmail.com>
2023-07-14 17:04:51 +0400

    deleted unused code

--- app/src/main/java/com/movies/MainActivity.kt
+++ app/src/main/java/com/movies/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.movies
 
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.movies.databinding.ActivityMainBinding
-import com.movies.databinding.ActivitySplashScreenBinding
 import com.movies.presentation.ui.HomeFragment
 
 class MainActivity : AppCompatActivity() {
@@ -19,24 +16,7 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen()
         setContentView(binding.root)
 
-//        showSplashScreen()
         fragmentSetup()
-    }
-
-    private fun showSplashScreen() {
-        val splashScreenLayout = ActivitySplashScreenBinding.inflate(layoutInflater)
-        binding.constraintFrame.addView(
-            splashScreenLayout.root,
-            ConstraintLayout.LayoutParams.MATCH_PARENT,
-            ConstraintLayout.LayoutParams.MATCH_PARENT
-        )
-
-        Handler().postDelayed({
-            binding.constraintFrame.removeView(splashScreenLayout.root)
-
-            fragmentSetup()
-            finish()
-        }, splashDuration)
     }
 
     private fun fragmentSetup() {
