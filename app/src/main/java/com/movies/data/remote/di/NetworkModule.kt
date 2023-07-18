ed02026bd8808e64e45dbd62d66347e3e9f0ad5b
davittsiskarashvili <datotsiskarashvili@gmail.com>
2023-07-18 18:10:39 +0400

    moved constants together

--- app/src/main/java/com/movies/data/remote/di/NetworkModule.kt
+++ app/src/main/java/com/movies/data/remote/di/NetworkModule.kt
@@ -1,5 +1,7 @@
 package com.movies.data.remote.di
 
+import com.movies.data.remote.di.ApiKey.BASE_URL
+import com.movies.data.remote.di.ApiKey.VALUE
 import com.movies.data.remote.service.api.ServiceApi
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
@@ -7,13 +9,10 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-
-private const val BASE_URL = "https://api.themoviedb.org/"
-
 private fun createInterceptor(): Interceptor {
     return Interceptor { chain ->
         val request = chain.request().newBuilder()
-            .header("Authorization", ApiKey.VALUE)
+            .header("Authorization", VALUE)
             .build()
         chain.proceed(request)
     }
@@ -38,6 +37,7 @@ val networkModule = module {
 }
 
 object ApiKey {
+    const val BASE_URL = "https://api.themoviedb.org/"
     const val VALUE =
         "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NmQ2OGQ3ZjRhMGUzZGNjZmE0NWU0ZTk3YjQ1ZjM3OSIsInN1YiI6IjYzMDEwMWQ5N2Q0MWFhMDA3OWJkNzYwMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eBH951remuFztKssncwnramEF6gdsVu350VI872cuwU"
 }
