811b0e8aa28694f5123388bdf292c0483cf17dc4
davittsiskarashvili <datotsiskarashvili@gmail.com>
2023-08-21 11:07:27 +0400

    deleted unused parameter

--- app/src/main/java/com/movies/presentation/activity/MoviesActivity.kt
+++ app/src/main/java/com/movies/presentation/activity/MoviesActivity.kt
@@ -17,7 +17,7 @@ class MoviesActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportFragmentManager.beginTransaction().add(
-            R.id.nav_host_fragment, HomeFragment()
+            R.id.nav_host_fragment, HomeFragment(), "home"
         ).commit()
     }
 
